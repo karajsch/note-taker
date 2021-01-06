b6770e8ce65d08280ea9f9056c507e104a59904c
Kara Schmidt <kara.jane.schmidt@gmail.com>
2021-01-05 19:49:32 -0500

    First commit to heroku

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const fs = require("fs");
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({
+    extended: true
+}));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+
+require('./routes/api_routes')(app);
+require("./routes/html_routes")(app)
+
+app.listen(PORT, function () {
+    console.log("App listening on PORT: " + PORT);
+});
